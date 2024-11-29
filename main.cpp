209676828c2041da1994142b309d508c8bb239ba
vicente <vicente2.0luis@gmail.com>
2024-11-28 18:28:29 -0600

    filto protocolo

--- main.cpp
+++ main.cpp
@@ -36,6 +36,8 @@ vector<packetData> packets;
 int max_y, max_x;
 int packetCount = 0;
 steady_clock::time_point start_time, end_time;
+string selected_protocol="ALL";
+
 
 int current_selection = 0;
 int scroll_start = 0;
@@ -49,6 +51,7 @@ void *threadpcap(void *arg);
 void packetManager(u_char *user, const struct pcap_pkthdr *pkthdr, const u_char *packetd_ptr);
 void draw_list();
 void move_selection(int direction);
+void select_protocol();
 
 int main(int argc, char const *argv[]) {
     pcap_if_t *alldevsp , *device;
@@ -110,7 +113,7 @@ int main(int argc, char const *argv[]) {
         }
         mvprintw(max_y - 1, max_x - 1, "%d", index);
     }
-
+    select_protocol();
     char error_buffer[PCAP_ERRBUF_SIZE];
     // devname = devs[0];
     pcap_t *capdev = pcap_open_live(devname, 65535, 1, -1, error_buffer);
@@ -185,6 +188,12 @@ int main(int argc, char const *argv[]) {
                 win1->init();
                 win1->refresh();
                 break;
+            case 'f':
+                select_protocol();
+                current_selection=0;
+                scroll_start=0;
+                draw_list();
+                break;
         }
         // mvprintw(max_y - 2, max_x - 10, "%6d", current_selection);
         // mvprintw(max_y - 1, max_x - 10, "%6d", scroll_start);
@@ -224,12 +233,65 @@ void *threadpcap(void *arg) {
     return NULL;
 }
 
+void select_protocol(){
+    const char* protocols[]={"ALL","TCP","UDP","ICMP","IP"};
+    int num_protocols=5;
+    int index=0;
+    int keyPressed=' ';
+
+    while(keyPressed!='\n'){
+        clear();
+        mvprintw(0,0,"select a protocol to filter plis : (if you dont press anything you arent a deity )");
+        for(int i=0; i<num_protocols;i++){
+            if(i==index){
+                cmvprintw(1,i+2,0,"%-10s",protocols[i]);
+            }else{
+                mvprintw(i+2,0,"%-10s",protocols[i]);
+
+            }
+        }
+        mvprintw(num_protocols+3,0,"Press Enter for path of deities:  Up/Down to navigate");
+        keyPressed=getch();
+        switch(keyPressed){
+            case 'W':
+            case 'w':
+            case KEY_UP:
+                index =(index-1 + num_protocols)% num_protocols;
+                break;
+            case 'S':
+            case 's':
+            case KEY_DOWN:
+                index=(index+1)% num_protocols;
+                break;
+        }
+    }
+    selected_protocol=protocols[index];
+    clear();
+    refresh();
+
+
+}
+
 void draw_list() {
     int available_rows = win1->max_y;
+    int displayed_packets=0;
+    int option_index=0;
 
-    for (int i = 0; i < available_rows; ++i) {
-        int option_index = scroll_start + i;
 
+    for(size_t i=scroll_start; i<packets.size() && displayed_packets < available_rows; ++i ){
+        const struct ip* ip_header=(struct ip*)(packets[i].data.data() +14);
+        int protocol=ip_header ->ip_p;
+        string protText;
+        switch (protocol) {
+            case IPPROTO_TCP: protText = "TCP"; break;
+            case IPPROTO_UDP: protText = "UDP"; break;
+            case IPPROTO_ICMP: protText = "ICMP"; break;
+            case IPPROTO_IP: protText = "IP"; break;
+            default: protText = to_string(protocol); break;
+        }
+        if (selected_protocol != "ALL" && selected_protocol != protText) {
+            continue; // Ignora este paquete
+        }
         if (option_index < packets.size()) {
             if (option_index == current_selection) {
                 wattron(win1->win, A_REVERSE);
@@ -274,25 +336,58 @@ void draw_list() {
             // Obtener tamaño del paquete
             int packet_size = ntohs(ip_header->ip_len);
 
-            mvwprintw(win1->win, i + 1, 1, "%5d %11f  %-15s %-15s  %4s  %6d", option_index, packets[option_index].elapsed_seconds.count(), source_ip, dest_ip, protText.c_str(), packet_size);
-            wattroff(win1->win, A_REVERSE);
+
+            
         }
+        if(displayed_packets == current_selection-scroll_start){
+            wattron(win1->win,A_REVERSE);
+        }
+
+        char source_ip[INET_ADDRSTRLEN], dest_ip[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET,&(ip_header->ip_src),source_ip,INET_ADDRSTRLEN);
+        inet_ntop(AF_INET,&(ip_header->ip_dst), dest_ip, INET_ADDRSTRLEN);
+        mvwprintw(win1->win, displayed_packets+1,1,"%5zu %11f  %-15s %-15s  %4s  %6d",i,packets[i].elapsed_seconds.count(),source_ip,dest_ip,protText.c_str(),ntohs(ip_header->ip_len));
+
+        if(displayed_packets == current_selection -scroll_start){
+            wattroff(win1->win,A_REVERSE);
+        }
+        ++displayed_packets;
+
     }
+
     win1->refresh();
 }
 
 void move_selection(int direction) {
     int new_selection = current_selection + direction;
+    while (new_selection >=0 && new_selection<static_cast<int>(packets.size())){
+        const struct ip* ip_header =(struct ip*)(packets[new_selection].data.data()+14);
+        int protocol=ip_header->ip_p;
 
-    if (new_selection >= 0 && new_selection < static_cast<int>(packets.size())) {
-        current_selection = new_selection;
+        string proText;
+        switch (protocol){
+            case IPPROTO_TCP: proText="TCP";break;
+            case IPPROTO_UDP: proText="UDP";break;
+            case IPPROTO_ICMP: proText="ICMP";break;
+            case IPPROTO_IP: proText="IP"; break;
+            default: proText=to_string(protocol);break;
+        }
+
+        if(selected_protocol == "ALL" || selected_protocol ==proText){
+            current_selection=new_selection;
+            break;
+        }
+        new_selection+= direction;
+
     }
 
-    if (current_selection < scroll_start) {
-        scroll_start = current_selection;
-    } else if (current_selection >= scroll_start + win1->max_y) {
-        scroll_start = current_selection - win1->max_y + 1;
+    if(current_selection < scroll_start){
+        scroll_start=current_selection;
+
+    }else if(current_selection >= scroll_start +win1->max_y){
+        scroll_start=current_selection -win1->max_y+1;
     }
+
 
     draw_list();
 }
